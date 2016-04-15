3a5a0d3e72e1878466f56716a08f0014e4eb3ffb
moon05 <amamun@u.rochester.edu>
2016-04-15 04:51:08 -0400

    new type of top sort

--- gibbs_sampling.py
+++ gibbs_sampling.py
@@ -1,5 +1,34 @@
 from random import uniform
 
+from collections import deque
+ 
+ 
+def kahn_topsort(graph):
+	in_degree = { u : 0 for u in graph }     # determine in-degree 
+	for u in graph:                          # of each node
+		for v in graph[u]:
+			in_degree[v] += 1
+			
+	Q = deque()                 # collect nodes with zero in-degree
+	for u in in_degree:
+		if in_degree[u] == 0:
+			Q.appendleft(u)
+
+	L = []     # list for order of nodes
+
+	while Q:                
+		u = Q.pop()          # choose node of zero in-degree
+		L.append(u)          # and 'remove' it from graph
+		for v in graph[u]:
+			in_degree[v] -= 1
+			if in_degree[v] == 0:
+				Q.appendleft(v)
+
+	if len(L) == len(graph):
+		return L
+	else:                    # if there is a cycle,  
+		return []            # then return an empty list
+
 #returns the normalized distribution of P(X | e)
 def gibbs_ask(X, e, bn, NSample):
 	N = [0] * len(bn.vars_dict[X])
